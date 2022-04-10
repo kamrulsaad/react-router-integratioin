0c92e3d36f9d6cccd8533bd448f7a074404d1abc
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-04-11 02:31:57 +0600

    Used react firebase - hooks

--- src/Components/Header/Login/Login.js
+++ src/Components/Header/Login/Login.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import { getAuth, signOut } from "firebase/auth";
+import React from "react";
+import app from "../../../firebase.init";
+import { useAuthState, useSignInWithGoogle } from 'react-firebase-hooks/auth'
+
+const auth = getAuth(app);
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login Page</h1>
-        </div>
-    );
+
+    const [user] = useAuthState(auth);
+
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+  return (
+    <div>
+      <h1>Please Login</h1>
+      <div>
+        <p>{user?.uid && user.displayName}</p>
+
+        {user?.uid ? (
+          <button onClick={() => signOut(auth)}>Sign Out</button>
+        ) : (
+          <button onClick={() => signInWithGoogle()}> Sign in with Google </button>
+        )}
+        <br />
+        <input type="text" placeholder="Enter your email" />
+        <br />
+        <input type="password" placeholder="Enter Password" />
+        <br />
+        <input type="button" value="Submit" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,17 +1,27 @@
+import { getAuth, signOut } from "firebase/auth";
 import React from "react";
-import useFirebase from "../../hooks/useFirebase";
+import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+
+const auth = getAuth(app)
 
 const Register = () => {
-  const { googleSignIn, user, handleSignOut } = useFirebase();
+
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+  const [user] = useAuthState(auth);
 
   return (
     <>
       <h1>Please Register</h1>
       <div>
+
+        <p>{user?.uid && user.displayName}</p>
+
         {user?.uid ? (
-          <button onClick={handleSignOut}>Sign Out</button>
+          <button onClick={() => signOut(auth)}>Sign Out</button>
         ) : (
-          <button onClick={googleSignIn}> Sign in with Google </button>
+          <button onClick={() => signInWithGoogle()}> Sign in with Google </button>
         )}
         <br />
         <input type="text" placeholder="Enter your email" />
